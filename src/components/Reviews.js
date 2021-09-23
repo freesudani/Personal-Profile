d83f445db1312b1b191390162aed5fcbf4c15afc
freesudani <mohamed.dahiya@hotmail.com>
2021-09-23 20:28:37 +0700

    Styling Review Section

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -2,6 +2,8 @@ import React from "react";
 import people from "../data/reviewData";
 import { useGlobalContext } from "./context";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { ParagVariant } from "./Variants";
 import "./Reviews.css";
 
 const Reviews = () => {
@@ -36,28 +38,38 @@ const Reviews = () => {
     setIndex(checkNumber(randomNumber));
   };
   return (
-    <article className="review">
-      <div className="img-container">
-        <img src={img} alt={name} className="person-img" />
-        <span className="quote-icon">
-          <FaQuoteRight />
-        </span>
-      </div>
-      <h4 className="author">{name}</h4>
-      <p className="job">{job}</p>
-      <p className="info">{text}</p>
-      <div className="button-container">
-        <button className="prev-btn" onClick={prevPerson}>
-          <FaChevronLeft />
+    <div className="reviews">
+      <article className="review">
+        <div className="img-container">
+          <img src={img} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h2 className="author">{name}</h2>
+        <h3 className="job">{job}</h3>
+        <motion.h3
+          className="info"
+          variants={ParagVariant}
+          initial="hidden"
+          animate="visible"
+          transition={{ ease: "easeIn", duration: 2.5 }}
+        >
+          {text}
+        </motion.h3>
+        <div className="button-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn" onClick={randomPerson}>
+          random
         </button>
-        <button className="next-btn" onClick={nextPerson}>
-          <FaChevronRight />
-        </button>
-      </div>
-      <button className="random-btn" onClick={randomPerson}>
-        surprise me
-      </button>
-    </article>
+      </article>
+    </div>
   );
 };
 
